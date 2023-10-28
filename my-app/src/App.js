db5d546a807f62956b8f2893ea020b15f31d38f2
GH <gh.im@hicomp.net>
2023-10-28 22:15:56 +0900

    Simple To Do List

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+// import Button from '@mui/material/Button';
+// import ButtonGroup from '@mui/material/ButtonGroup';
+// import Container from '@mui/material/Container';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    
+    if(todo === ""){
+      return;
+    }
+
+    setTodo("");
+    setTodos((currentArray) => [todo, ...currentArray]);
+    
+  };
+
+  //console.log(todos);
+
+  const onClick = () => {
+
+  };
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Container fixed>
+    //   <ButtonGroup variant="contained" aria-label="outlined primary button group">
+    //     <Button>TEXT</Button>
+    //     <Button>TEXT</Button>
+    //   </ButtonGroup>
+    // </Container>
+    <div>
+      <h1>My To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          placeholder="TODOLIST"
+          onChange={onChange}
+          value={todo}
+        />
+        <button onClick={onClick}>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
